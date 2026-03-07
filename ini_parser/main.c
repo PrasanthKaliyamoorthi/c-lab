9020b0a7cb3d0f54db13d26add7412563f32a070
Prasanth Kalaiyamoorthi <prasanthkaliyamoorthi2000@gmail.com>
2026-03-07 09:14:45 +0530

    ini parser read line by line

--- ini_parser/main.c
+++ ini_parser/main.c
@@ -11,24 +11,48 @@ typedef struct {
   bool istrue;
 } config;
 
+void rtrim(char *s) {
+    int i = strlen(s) - 1;
+    while (i >= 0 && isspace((unsigned char)s[i]))
+        s[i--] = '\0';
+}
 
 void generate_conf(char* filename) {
   puts("Generating example config file. `config.ini.sample`.");
-  puts("Old `config.ini.sample` file will be overwritten. Y/n");
 
-  char choice = getchar();
-  if ((char) tolower(choice) == 'n') {
-    exit(0);
-  }
+  FILE* fr = fopen("./config.ini.sample", "r");
+  if (fr != NULL) {
+    puts("Old `config.ini.sample` file will be overwritten. Y/n");
+    char choice = getchar();
+    if ((char) tolower(choice) == 'n') {
+      exit(0);
+    };
+  };
 
+
+  FILE* fd = fopen("./config.ini.sample", "w");
   puts("After configuring the sample file rename it to `config.ini`.");
-  FILE* fd = fopen("./config.ini.sample", "a");
 
   fprintf(fd, "#--- this is a sample config file after done editing remove this comment ---\n");
   fprintf(fd, "name = \"user\"\n");
   fprintf(fd, "age = 77\n");
   fprintf(fd, "alive = true\n");
   exit(0);
+}
+
+bool validate_conf(FILE* conf) {
+  char* line = NULL;
+  size_t len = 0;
+  ssize_t read_len;
+
+  while ((read_len = getline(&line, &len, conf)) != -1) {
+    rtrim(line);
+    printf("%s\n", line);
+    printf("read length : %ld\n", read_len);
+    puts("");
+  }
+  
+  return true;
 }
 
 
@@ -40,5 +64,10 @@ int main(int argc, char* argv[]) {
     puts("Config file is missing from current directory.");
     generate_conf("config.ini.sample");
   };
+  if (!validate_conf(fd)) {
+    puts("Config error. config validation error.!");
+    exit(-1);
+  };
+
   return EXIT_SUCCESS;
 }
